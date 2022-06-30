cdbd2879790b75a031fbd728e74d97bdd48e90cc
Josh Smith <jasbuckmaster@gmail.com>
2022-06-30 16:09:13 -0400

    Removing docker project reference

--- Web/Program.cs
+++ Web/Program.cs
@@ -7,15 +7,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace API
+namespace Web
 {
     public class Program
     {
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
-
-            var addition = 1 + 2;
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
