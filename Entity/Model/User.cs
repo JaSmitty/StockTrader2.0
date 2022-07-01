0c5f437edb5b5ccfcd60e9c6ef5b7b8284df9d0b
Josh Smith <jasbuckmaster@gmail.com>
2022-07-01 13:41:02 -0400

    Adding start of user models

--- Entity/Model/User.cs
+++ Entity/Model/User.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace Entity.Model
 {
-    public class User
+    public class User : BaseEntity
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
     }
 }
